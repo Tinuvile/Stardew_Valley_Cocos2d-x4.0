8716516a6397f3381feebccabdf38aa479ab0bca
FLicker-514 <3478448962@qq.com>
2024-11-27 17:53:25 +0800

    Add files via upload
    提交小镇场景的代码

---
+++ Classes/Town.h
@@ -0,0 +1,50 @@
+#ifndef __Town_H__
+#define __Town_H__
+
+#include "cocos2d.h"
+#include "Player.h"
+#include "AppDelegate.h"
+#include "physics/CCPhysicsWorld.h"
+
+class myhouse : public cocos2d::Scene
+{
+public:
+
+    myhouse();
+    ~myhouse();
+
+    virtual bool init();
+
+    static myhouse* create();
+
+    Player* player1;
+
+    // 退出按钮的回调函数，点击退出按钮时执行的操作
+    virtual void menuCloseCallback(cocos2d::Ref* pSender);
+
+    // 判断角色的位置
+    void checkPlayerPosition(Player* player);
+    
+    // 创建一个列表，用于保存所有非透明像素的坐标
+    std::vector<cocos2d::Vec2> nonTransparentPixels;
+
+    // 创建房子的区域
+    cocos2d::Rect Get_Task = cocos2d::Rect(473, 578, 120, 30);  // 领取任务区域
+
+private:
+
+    // 用于显示玩家位置的标签
+    cocos2d::Label* _positionLabel;
+
+    // 用于显示计时的标签
+    cocos2d::Label* _timerLabel;  
+
+    cocos2d::Sprite* In_gettask;
+
+
+
+    cocos2d::PhysicsWorld* _physicsWorld;
+
+};
+
+#endif // __BACKGROUND1_H__
