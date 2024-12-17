8954f8ee38b05ca0fc228458a57f7abb4b1acba4
Tinuvile <3145593233@qq.com>
2024-12-18 00:23:25 +0800

    Reapply "AppDelegate"
    This reverts commit b97643f5ad0a060d37f675b2964ad10187f7cb83.

--- Classes/AppDelegate.h
+++ Classes/AppDelegate.h
@@ -31,6 +31,7 @@
 #include "Cow.h"
 #include "Sheep.h"
 
+#include "TaskManagement.h"
 #include "NPC.h"
 #include "NPCreate.h"
 #include "NPCtalkUI.h"
@@ -60,6 +61,7 @@ class Barn;
 class Inventory;
 class NpcRelationship;
 class mini_bag;
+class TaskManagment;
 /******************************** 全局变量声明区 ***************************************/
 extern int remainingTime;  // 声明，不初始化
 extern int day;
@@ -93,6 +95,8 @@ extern SkillTree* skill_tree;
 extern mini_bag* miniBag;
 extern Inventory* inventory;
 extern NpcRelationship* npc_relationship;
+extern Inventory* StoreItem;
+extern TaskManagement* taskManager;
 //存储畜棚中可有家畜活动的矩形区域，并记录该区域是否已存在家畜
 extern std::vector<std::pair<Rect , bool>> barn_space;
 //存储畜棚中的家畜
