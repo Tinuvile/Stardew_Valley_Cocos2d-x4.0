a096479fc9e817c6ae2cfb217bc897109ab165d0
Tinuvile <3145593233@qq.com>
2024-12-19 02:27:12 +0800

    修改

--- Classes/AppDelegate.h
+++ Classes/AppDelegate.h
@@ -61,11 +61,8 @@ class Barn;
 class Inventory;
 class NpcRelationship;
 class mini_bag;
-<<<<<<< HEAD
 class TaskManagement;
-=======
 class Timesystem;
->>>>>>> b9a0d5c05becfb4e1a570e81ad94622ddc23bcec
 /******************************** 全局变量声明区 ***************************************/
 extern int remainingTime;  // 声明，不初始化
 extern int day;
@@ -101,12 +98,9 @@ extern SkillTree* skill_tree;
 extern mini_bag* miniBag;
 extern Inventory* inventory;
 extern NpcRelationship* npc_relationship;
-<<<<<<< HEAD
 extern Inventory* StoreItem;
 extern TaskManagement* taskManager;
-=======
 extern Timesystem* TimeUI;
->>>>>>> b9a0d5c05becfb4e1a570e81ad94622ddc23bcec
 //存储畜棚中可有家畜活动的矩形区域，并记录该区域是否已存在家畜
 extern std::vector<std::pair<Rect , bool>> barn_space;
 //存储畜棚中的家畜
